22ddaa893f9284cf36cb5a31eef85748c70017d4
rokh0716 <jdi.s@mail.ru>
2019-05-24 20:55:03 +0400

    Half of first lesson

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -1,8 +1,8 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
-import { HeroesComponent } from './heroes/heroes.component';
+import {AppComponent} from './app.component';
+import {HeroesComponent} from './heroes/heroes.component';
 
 @NgModule({
   declarations: [
@@ -15,4 +15,5 @@ import { HeroesComponent } from './heroes/heroes.component';
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
