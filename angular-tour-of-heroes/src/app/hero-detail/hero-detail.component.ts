56c664e673ee333d8d28717285e71693578b6913
rokh0716 <jdi.s@mail.ru>
2019-06-11 18:10:57 +0400

    Lesson four has been done

--- angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
+++ angular-tour-of-heroes/src/app/hero-detail/hero-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, Input } from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {Hero} from "../hero";
 
 @Component({
@@ -9,7 +9,8 @@ import {Hero} from "../hero";
 export class HeroDetailComponent implements OnInit {
   @Input() hero: Hero;
 
-  constructor() { }
+  constructor() {
+  }
 
   ngOnInit() {
   }
